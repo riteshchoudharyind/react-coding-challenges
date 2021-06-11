253e1e70295df28a01d90e112bd0d08190893d87
riteshchoudhary <riteshchoudharyind@gmail.com>
2021-06-11 20:40:57 +0530

    Initial api calls

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    clientId: '7e163d336cfb44e1a714afcbe9e47fa7',
+    clientSecret: '58b369dcbff04bb68abbee459485f46f'
   }
 }
--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
+import SpotifyWebApi from 'spotify-web-api-js';
 import '../styles/_discover.scss';
+import api from '../../../config';
 
 export default class Discover extends Component {
+  accessToken= 'BQDd8_RHG6X5-26JwAoE2dP1iAykG1MuAyeLC62ZX5gbUdIJTyIdwfP2Tc355bhnD0mblT6Gtwn-G3hJTwLXoIk7tehfJtXtMnjg3GAv6E3-dgEa9ALFZ8rYwFgWRYIHahg8muZJTx0utG-AmXbBemSiSbqnHbhiVd1DtEI';
+  spotifyApi = new SpotifyWebApi();
+  getApi = api.baseUrl;
+  headObj = {
+    headers: new Headers({
+      'Authorization': 'Bearer ' + this.accessToken,
+    })
+  };
   constructor() {
     super();
 
@@ -11,6 +21,35 @@ export default class Discover extends Component {
       playlists: [],
       categories: []
     };
+  }
+
+  componentDidMount(){
+    fetch('https://api.spotify.com/v1/browse/new-releases?country=SE&offset=0&limit=20', this.headObj)
+    .then((res) => {
+      if(res) {
+        this.state.newReleases = res.albums.items;
+      }
+    }, (err) => {
+      console.log('Error Occure', err);
+    });
+
+    fetch('https://api.spotify.com/v1/browse/featured-playlists?country=SE&timestamp=2015-05-18T06:44:32&offset=0&limit=2', this.headObj)
+    .then((res) => {
+      if(res) {
+        this.state.playlists = res.playlists.items;
+      }
+    }, (err) => {
+      console.log('Error Occure', err);
+    });
+
+    fetch('https://api.spotify.com/v1/browse/categories?offset=0&limit=20', this.headObj)
+    .then((res) => {
+      if(res) {
+        this.state.categories = res.categories.items;
+      }
+    }, (err) => {
+      console.log('Error Occure', err);
+    });
   }
 
   render() {
